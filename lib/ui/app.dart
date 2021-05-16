1a6eb086139a2ce99964ca332764b8b14e05f7ff
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2021-05-16 10:48:21 +0200

    Display text informing user they can't use the app if their aspect ratio's too small. That's because I'm too lazy to make this thing responsive

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -4,13 +4,15 @@ import 'package:randomizer/utils/constants/color.const.dart';
 
 class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: COLOR_PRODUCT,
-          accentColor: COLOR_PRODUCT,
-          appBarTheme: AppBarTheme(elevation: 0.0),
-        ),
-        home: HomeScreen(),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: COLOR_PRODUCT,
+        accentColor: COLOR_PRODUCT,
+        appBarTheme: AppBarTheme(elevation: 0.0),
+      ),
+      home: HomeScreen(),
+    );
+  }
 }
